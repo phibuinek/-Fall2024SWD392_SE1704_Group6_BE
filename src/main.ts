ea9277f8b69706cebe1de9a0c99dd6f916f385db
MTh1enn <thientnmse1735541@fpt.edu.vn>
2024-09-26 18:04:09 +0700

    Set up docker

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+
+  // Xuất tài liệu Swagger ra tệp JSON
+  writeFileSync('./swagger.json', JSON.stringify(document));
   await app.listen(3000);
 }
 bootstrap();
